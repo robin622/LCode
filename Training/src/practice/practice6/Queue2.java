643f29d13afd2b40b964e7be0d46d065dfe4b608
wei1.z <wei1.z@samsung.com>
2019-08-02 09:34:29 -0500

    practice circular array

--- Training/src/practice/practice6/Queue2.java
+++ Training/src/practice/practice6/Queue2.java
@@ -1,11 +1,11 @@
 package practice.practice6;
 
 //using array
-public class Queue2 {
-    private int[] arr;
-    private int size;
-    private int head;
-    private int tail;
+//public class Queue2 {
+//    private int[] arr;
+//    private int size;
+//    private int head;
+//    private int tail;
     
 //    public Queue2(int cap) {
 //        arr = new int[cap];
@@ -33,6 +33,55 @@ public class Queue2 {
 //    public int size() {
 //
 //    }
+public class Queue2 {
+    
+    //using circular array to implement a queue
+    private int head;
+    private int tail;
+    private int size;
+    private int[] array;
+    
+    public Queue2(int capacity) {
+        array = new int[capacity];
+        head = 0;
+        tail = 0;
+        size = 0;
+    }
+    
+    public boolean offer(int value) {
+        if(size == array.length) {
+            return false;
+        }
+        array[head] = value;
+        head = (head + 1) % array.length;
+        size++;
+        return true;
+    }
+    
+    public Integer poll() {
+        if(size == 0) {
+            return null;
+        }
+        int ret = array[tail];
+        tail = (tail + 1) % array.length;
+        size--;
+        return ret;
+    }
+    
+    public Integer peek() {
+        if(size == 0) {
+            return null;
+        }
+        return array[tail];
+    }
+    
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+    
+    public int size() {
+        return size;
+    }
     
     public static void main(String[] args) {
         String[] expectedArr = "41C,41D,25A25A,".split(",");
@@ -41,5 +90,6 @@ public class Queue2 {
         }
         
     }
-
 }
+
+//}
